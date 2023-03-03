c8e86efe31d3f5473da591705ffa34b47552dbbb
Sergio Pelegrini <serpel@gmail.com>
2023-03-03 11:30:58 -0300

    first commit

---
+++ src/br/com/serpel/PrimeiroPrograma.java
@@ -0,0 +1,27 @@
+package br.com.serpel;
+
+import br.com.serpel.model.Gato;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+
+		Gato gato = new Gato();
+		Livros livros = new Livros();
+		
+		System.out.println(gato);
+		System.out.println(livros);
+		
+		/*int a = 2;
+		int b = 5;
+		System.out.println("Hello World! " + (a + b));*/
+		
+		
+
+	}
+}
+
+class Livros {
+	private String nome;
+	private String npag;
+}
